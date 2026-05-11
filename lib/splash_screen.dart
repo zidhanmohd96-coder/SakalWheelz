9ed7fb9bf0a33eebfc7fbba3339184e7e6674772
ZiMu <zidhanmohd96@gmail.com>
2026-05-11 21:41:17 +0530

    Update comment for login status check

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -45,7 +45,7 @@ class _SplashScreenState extends State<SplashScreen>
     await Future.delayed(const Duration(seconds: 2));
 
     if (mounted) {
-      // 2. CHECK LOGIN STATUS HERE
+      // 2. Check Something
       User? user = FirebaseAuth.instance.currentUser;
 
       if (user != null) {
